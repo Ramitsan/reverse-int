ec70e04a7b05e2f8beabe6d52da56e829c8f7889
Ramitsan <ramitsan@gmail.com>
2020-10-04 00:35:29 +0300

    feat: add the solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  if (n < 0) {
+    n = Math.abs(n);
+  }
+  let str = String(n);
+  return str.split("").reverse().join("");
 }
